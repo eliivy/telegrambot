8074385caae5a8b8f042f53eabfe73405821f5fc
eliivy <48825784+eliivy@users.noreply.github.com>
2019-05-11 14:46:12 +0300

    Update working.py

--- working.py
+++ working.py
@@ -4,7 +4,6 @@ import time as t
 
 liza_id = 166934167
 token = '857333265:AAHwUpghmbxCwKnqVUE_kHzNz1NijpRqYHY'
-
 bot = telebot.TeleBot(token)
 
 
@@ -18,67 +17,36 @@ def handle_start(message):
 @bot.message_handler(content_types=['text'])
 def handle_level(message):
     if message.text == 'Новичок':
-        with open('trains.json', 'r') as f:
-            training = json.load(f)
-        img = open('beginner/pic1.png', 'rb')
-        day = 1
-        bot.send_message(message.from_user.id, training['Новичок{0}'.format(str(day))])
-        bot.send_photo(message.from_user.id, img)
-        img.close()
-        sendingtime = '00'
-        while 1:
-            now = t.strftime("%S", t.localtime())
-            if now == sendingtime:
-                day += 1
-                bot.send_message(message.from_user.id, training['Новичок{0}'.format(str(day))])
-                img = open('beginner/pic{0}.png'.format(str(day)), 'rb')
-                bot.send_photo(message.from_user.id, img)
-                img.close()
-                if day == 30:
-                    break
-            t.sleep(1)
-    elif message.text == 'Любитель':
-        with open('trains.json', 'r') as f:
-            training = json.load(f)
-        img = open('average/pic1.png', 'rb')
-        day = 1
-        bot.send_message(message.from_user.id, training['Средний{0}'.format(str(day))])
-        bot.send_photo(message.from_user.id, img)
-        img.close()
-        sendingtime = '00'
-        while 1:
-            now = t.strftime("%S", t.localtime())
-            if now == sendingtime:
-                day += 1
-                bot.send_message(message.from_user.id, training['Средний{0}'.format(str(day))])
-                img = open('average/pic{0}.png'.format(str(day)), 'rb')
-                bot.send_photo(message.from_user.id, img)
-                img.close()
-                if day == 30:
-                    break
-            t.sleep(1)
-    elif message.text == 'Профессионал':
-        with open('trains.json', 'r') as f:
-            training = json.load(f)
-        img = open('professional/pic1.png', 'rb')
-        day = 1
-        bot.send_message(message.from_user.id, training['Профессионал{0}'.format(str(day))])
-        bot.send_photo(message.from_user.id, img)
-        img.close()
-        sendingtime = '00'
-        while 1:
-            now = t.strftime("%S", t.localtime())
-            if now == sendingtime:
-                day += 1
-                bot.send_message(message.from_user.id, training['Профессионал{0}'.format(str(day))])
-                img = open('professional/pic{0}.png'.format(str(day)), 'rb')
-                bot.send_photo(message.from_user.id, img)
-                img.close()
-                if day == 30:
-                    break
-            t.sleep(1)
+        level = "beginner"
+
+    if message.text == 'Любитель':
+        level = "average"
+
+    if message.text == 'Профессионал':
+        level = "professional"
+
+    with open('trains.json', 'r') as f:
+        training = json.load(f)
+    img = open('{0}/pic1.png'.format(level), 'rb')
+    day = 1
+    bot.send_message(message.from_user.id, training['{1}{0}'.format(str(day), level)])
+    bot.send_photo(message.from_user.id, img)
+    img.close()
+    sendingtime = '00'
+    while 1:
+        now = t.strftime("%S", t.localtime())
+        if now == sendingtime:
+            day += 1
+            bot.send_message(message.from_user.id, training['{1}{0}'.format(str(day), level)])
+            img = open('{1}/pic{0}.png'.format(str(day), level), 'rb')
+            bot.send_photo(message.from_user.id, img)
+            img.close()
+            if day == 30:
+                break
+        t.sleep(1)
 
 
-bot.polling(none_stop=True, interval=0)
 
 
+if __name__ == "__main__":
+    bot.polling(none_stop=True, interval=0)
